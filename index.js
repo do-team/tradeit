b68aef39be8a461161278c4b98f9c0e2683eaced
kuritka <kuritka@gmail.com>
2016-10-12 22:48:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,25 @@
 var mysql = require('mysql');
+var common = require('./common');
 
 
-exports.handler = function(event, context) {
-
-
-function GetCommand(str, context){
-            var data = str.toUpperCase().split(' ');
-            if(data.length != 3)
-            {
-                context.fail("put command in correct format [buy/sell]] [member] [value] '" + str +"'");
-                return null;
-            }
-            else{
-                return  new {
-                    command: data[0],
-                    member: data[1],
-                    value: data[2]
-                }
-            }
-    };
-
-
+exports.handler = function(event, context) 
+{
     if (event === null || event.text === null) {
         context.succeed('You sent nothing!');
     }
     else 
     {
-        var data = GetCommand(event.text, context);
+        var data = common.parseInputOrder(event.text);
 
-    //     var connection =  mysql.createConnection({
-    //       host   : 'futuredb.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
-    //       user   : 'marty',
-    //       password : 'martymarty',
-    //       database : 'Member'
-    //     });
-    //    connection.connect(function(err){
-    //             if(err){
-    //                 context.fail('Database connection failed' + err);
-    //             }    
-    //             else
-    //             {
-                    switch(data.command){
-                        case 'BUY':
-                            context.succeed('insert into table (position, member , value) values (buy,'+data.member+','+data.value+')')
-                        break;
-                        case 'SELL':
-                            context.succeed('insert into table (position, member , value) values (sell,'+data.member+','+data.value+')')
-                        break;
-                    }
-                    context.fail('Unexpected command' + data.command );
-       //         }
-       //     });
-       // }
-};
+        switch(data.command){
+            case 'BUY':
+                context.succeed('insert into table (position, member , value) values (buy,'+data.member+','+data.value+')')
+            break;
+            case 'SELL':
+                context.succeed('insert into table (position, member , value) values (sell,'+data.member+','+data.value+')')
+            break;
+        }
+        context.fail('Unexpected command' + data.command );
+    };
+}
 
