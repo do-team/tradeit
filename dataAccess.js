27a0be39fb2e0861ffc9ad6290aa181d12d354c7
David Kubec <davesade.42@gmail.co,m>
2016-10-14 21:23:29 +0200

    Nove SQL selecty!

--- dataAccess.js
+++ dataAccess.js
@@ -15,3 +15,66 @@ exports.getProductNames = function(callback)
 
         sqlBase.getStaticData(query, callback);
 }
+
+exports.getAskPrices = function(callback)
+{
+        var query = "SELECT * FROM orderbook WHERE product_name='WOOD' AND order_type='sell' ORDER BY price DESC";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.getBidPrices = function(callback)
+{
+        var query = "SELECT * FROM orderbook WHERE product_name='WOOD' AND order_type='buy' ORDER BY price ASC";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.deleteLowestBid = function(callback)
+{
+        var query = "DELETE FROM orderbook WHERE product_name='wood' AND order_type='buy' ORDER BY price ASC LIMIT 1";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.deleteHighestAsk = function(callback)
+{
+        var query = "DELETE FROM orderbook WHERE product_name='wood' AND order_type='sell' ORDER BY price DESC LIMIT 1";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.insertBid = function(callback)
+{
+        var query = "INSERT INTO orderbook (order_type, product_name, price) VALUES ('buy','wood',98)";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.insertAsk = function(callback)
+{
+        var query = "INSERT INTO orderbook (order_type, product_name, price) VALUES ('sell','wood',98)";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.countBids = function(callback)
+{
+        var query = "SELECT COUNT(*) FROM orderbook WHERE product_name='wood' AND order_type='buy'";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.countAsks = function(callback)
+{
+        var query = "SELECT COUNT(*) FROM orderbook WHERE product_name='wood' AND order_type='sell'";
+
+        sqlBase.getStaticData(query, callback);
+}
+
+exports.deleteMatchedOrders = function(callback)
+{
+        var query = "DELETE t1,t2 FROM orderbook t1, orderbook t2 WHERE t1.product_name = t2.product_name AND t1.price = t2.price AND t1.order_type <> t2.order_type";
+
+        sqlBase.getStaticData(query, callback);
+}
