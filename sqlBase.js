3a5230183a08ac87b75fd8b360474292b76dfb43
David Kubec <davesade.42@gmail.co,m>
2016-10-14 10:22:08 +0200

    Change database connection from var to function.

--- sqlBase.js
+++ sqlBase.js
@@ -1,67 +1,35 @@
 var mysql = require('mysql');
 
-
-//var connection =  mysql.createConnection({
-//            host   : 'futuredb.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
-//            user   : 'marty',
-//            password : 'martymarty',
-//            database : 'microexchange'
-//});
-
-function ConnectionStart()
+function connectionStart()
 {
         var connection = mysql.createConnection({
-         host     : 'futuredb.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
-         database : 'microexchange',
-         user     : 'marty',
-         password : 'martymarty',
-       });
-       connection.connect();
-       return connection;
+        host     : 'futuredb.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
+        database : 'microexchange',
+        user     : 'marty',
+        password : 'martymarty',
+        });
+        return connection;
 }
-
-//exports.getStaticData = function(sqlQuery, callback)
-//{
-//        connection.query(sqlQuery, function(err, rows, fields){
-//                if (err)
-//                {
-//                  callback(err,null);
-//                }
-//                else
-//                  callback(null, rows);
-//        });
-//        connection.end();
-//}
 
 exports.getStaticData = function(sqlQuery, callback)
 {
-        var connection = ConnectionStart();
+        var connection = connectionStart();
+        connection.connect();
         connection.query(sqlQuery, function(err, rows, fields){
-                if (err)
-                {
-                  callback(err,null);
-                }
-                else
-                  callback(null, rows);
-        });
-        connection.end();
+              if (err)
+              {
+                callback(err,null);
+              }
+              else
+                callback(null, rows);
+      });
+      connection.end();
 }
-
-
-//exports.getSingleRecord = function(sqlQuery, callback)
-//{
-//        connection.query(sqlQuery, function(err, rows, fields){
-//                if (err)
-//                  callback(err,null);
-//                else
-//                  callback(null, rows[0]);
-//        });
-//        connection.end();
-//}
 
 exports.getSingleRecord = function(sqlQuery, callback)
 {
         var connection = ConnectionStart();
+        connection.connect();
         connection.query(sqlQuery, function(err, rows, fields){
                 if (err)
                 {
