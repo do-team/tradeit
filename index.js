697c234575f69f192db93e433e0dd4363fb5c978
David Kubec <davesade.42@gmail.co,m>
2016-10-17 21:36:47 +0200

    Working data access template.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var _ = require('lodash');
 
 exports.handler = function(event, context) 
 {
-    if (event === null || event.text === null) {
+    if (event === null || event.text === null)
+     {
         context.succeed('You sent nothing!');
-    }
+     }
         if (event.text.toLowerCase() === 'products')
         da.getProductNames(function(err,data)
         {
@@ -16,9 +17,10 @@ exports.handler = function(event, context)
             else
                 {
                 var result = '';
-                _.forEach(data,function(value){
-                result+=value.product_name+' ';
-                });
+                _.forEach(data,function(value)
+                    {
+                    result+=value.product_name+', ';
+                    });
                     context.succeed('Available products: ' + result.toUpperCase());
                 }
         }) ;
@@ -26,27 +28,21 @@ exports.handler = function(event, context)
                 context.succeed('HELP recognised!');
 
      else
-    {
 
-context.succeed('Do not understand...');
+     da.getBidPrices(function(err,data)
+             {
+                 if(err !== null)
+                     context.fail(err);
+                 else
+                     {
+                     var result = '';
+                     _.forEach(data,function(value)
+                         {
+                         result+=value.price+', ';
+                         });
+                         context.succeed('Wood is being sold for ' + result.toUpperCase());
+                     }
+             }) ;
 
-     //example of usage DB
-     //   da.getBusinessIdByMember(data.member,function(err, data)
-     //  {
-     //    if(err !== null)
-     //           context.fail(err);
-     //     else
-     //           context.succeed('found member' + data.member);
-     //   });
+}
 
-     //   switch(data.command){
-     //      case 'BUY':
-     //           context.succeed('insert into table (position, member , value) values ('data.command+','+data.product+','+data.price+')');
-     //       break;
-     //       case 'SELL':
-     //           context.succeed('insert into table (position, member , value) values ('data.command','+data.product+','+data.price+')');
-     //       break;
-     //   }
-     //   context.fail('Unexpected command or argument' + data.command + data.product + data.price);
-    }
-};
