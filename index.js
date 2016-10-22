04b79700f28bca9f01e676a9b250d58a4b4df87c
David Kubec <davesade.42@gmail.co,m>
2016-10-22 14:02:23 +0200

    Experimenting...

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var common = require('./common');
 var _ = require('lodash');
 var market_depth = 5;
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context) { //1
     da.historyRecord(event); // This is saving complete history of any command sent by user
 
     if (event === null || event.text === null) {
         context.succeed('You sent nothing!'); // Basic protection
     }
-    if (event.text.toLowerCase() === 'products')
+    if (event.text.toLowerCase() === 'products') { // 2
         da.getProductNames(function(err, data) // Special command to display available products on market
             {
                 if (err !== null)
@@ -27,64 +27,58 @@ exports.handler = function(event, context) {
         context.succeed('HELP recognised!');
     if (event.text.toLowerCase() === 'test')
         context.succeed('TEST OK');
-    else
-
-        var data = common.parseInputOrder(event.text);
-    da.confirmCommand(data, function(err, commandRows) {
-        if (err !== null)
-            context.fail(err);
-        else {
-            if (commandRows == null) {
-                context.succeed('Order type ' + data.command + ' not available! Please try /TRD HELP first!');
-            } else {
-                // Add check, if order type / command exists here.
-                da.confirmProductAvailable(data, function(err, dataRows){ // Check, if product exists.
-                        if (err !== null)
-                            context.fail(err);
-                        else {
-                            if (dataRows == null) {
-                                context.succeed('Product ' + data.product + ' not available! Please try /TRD PRODUCTS to see, what is available.');
-                            } else {
-                                if (!data.price) {
-                                    da.getPrices(data, function(err, dataRows) {
-                                        if (err !== null)
-                                            context.fail(err);
-                                        else {
-                                            var result = '';
-                                            _.forEach(dataRows, function(value) {
-                                                result += value.price + ', ';
-                                            });
-                                            context.succeed(data.product + ' is available on ' + data.command + ' side for following prices: ' + result);
-                                        }
-                                    });
-                                } else {
-                                        da.insertOrder(data, function(err, dataRows) {
-                                            if (err !== null)
-                                                context.fail(err);
-                                                var countRows = 0;
-                                                da.countOrders(data, function(err, countRows) {
-                                                if (err !== null)
-                                                    context.fail(err);
+        else { // 3
+            var data = common.parseInputOrder(event.text);
+            da.confirmCommand(data, function(err, commandRows) { // 4
+                    if (err !== null)
+                        context.fail(err);
+                    else { // 5
+                        if (commandRows == null)
+                            context.succeed('Order type ' + data.command + ' not available! Please try /TRD HELP first!');
+                        else { // 6
+                            da.confirmProductAvailable(data, function(err, dataRows) { // 7
+                                    if (err !== null)
+                                        context.fail(err);
+                                    else { // 8
+                                        if (dataRows == null)
+                                            context.succeed('Product ' + data.product + ' not available! Please try /TRD PRODUCTS to see, what is available.');
+                                        else { // 9
+                                            if (!data.price) {
+                                                da.getPrices(data, function(err, dataRows) {
+                                                    if (err !== null)
+                                                        context.fail(err);
+                                                    else {
+                                                        var result = '';
+                                                        _.forEach(dataRows, function(value) {
+                                                            result += value.price + ', ';
+                                                        });
+                                                        context.succeed(data.product + ' is available on ' + data.command + ' side for following prices: ' + result);
+                                                    }
+                                                });
+                                            }
+                                            else { // 10
+                                                da.insertOrder(data, function(err, dataRows) { // 11
+                                                    if (err !== null)
+                                                        context.fail(err);
                                                     var countRows = 0;
+                                                    da.countOrders(data, function(err, countRows) {
+                                                        if (err !== null)
+                                                            context.fail(err);
+                                                        var countRows = 0;
                                                         if (countRows > market_depth) {
                                                             context.succeed('Order ' + data.command + ' ' + data.product + ' ' + data.price + ' is IN. COUNT: ' + countRows);
-                                                        }
-                                                        else {
+                                                        } else {
                                                             context.succeed('Order ' + data.command + ' ' + data.product + ' ' + data.price + ' is IN. COUNT: ' + countRows);
                                                         }
-                                                });
-
-                                        });
-
-                                   });
-
-
-                                // KONEC NOVEHO KODU
-                                }
-                            }
-                    }
-                });
-            }
-        }
-    });
-};
+                                                    });
+                                                }); // 11
+                                            } // 10
+                                        } // 9
+                                    } // 8
+                            }); // 7
+                        } // 6
+                    } // 5
+            }); // 4
+        } // 3
+    } // 2
+}; // 1
