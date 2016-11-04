10305db2ac15b0f1312d82adde8563257e8d5f5f
kubedav <davesade.42@gmail.com>
2016-11-04 15:45:17 +0100

    Polishing content, next: add promises.

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 This is attempt to have server less micro exchange - customer can send
 his order directly from chat (Slack), he will receive back a response.
 So it's not really a bot, "bot" only faces customer.
-TEST
--- test.js
+++
@@ -1,8 +0,0 @@
-var da = require('./dataAccess');
-
-exports.handler = function(event, context) {
-
-        da.historyRecordAlt(event, context); // This is saving complete history of any command sent by user, whatever string he sends.
-
-};
-
