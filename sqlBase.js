c8b602b1ff33cdfacd5e9bdb742e4e56cec60543
kubedav <davesade.42@gmail.com>
2016-11-07 12:53:08 +0100

    zkouska

--- sqlBase.js
+++ sqlBase.js
@@ -1,55 +1,54 @@
 var mysql = require('mysql');
+var Promise = require('promise');
 
-function connectionStart()
-{
+function connectionStart() {
         var connection = mysql.createConnection({
-        host     : 'microexchange.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
-        database : 'microexchange',
-        user     : 'microadmin',
-        password : 'micropassword',
+                host: 'microexchange.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
+                database: 'microexchange',
+                user: 'microadmin',
+                password: 'micropassword',
         });
         return connection;
 }
 
-exports.getStaticData = function(sqlQuery, callback, context)
-{
-        var connection = connectionStart();
-        connection.connect();
-        console.log('State 1 - before query ' + connection.state);
-        connection.query(sqlQuery, function(err, rows, fields){
-        console.log('State 2 - inside query ' + connection.state);
-              if (err)
-              {
-                console.log(err);
-                callback(err, null, context);
-              }
-              else {
-              callback(null, rows, context);
-              }
+exports.getStaticData = function (sqlQuery) {
+        // Create new promise
+        var promise = new Promise(function (resolve, reject) {
+                var connection = connectionStart();
+                connection.connect();
+                console.log('State 1 - before query ' + connection.state);
 
-      });
-    console.log('State 3 - after query ' + connection.state);
-    connection.end();
+                // create asynchronous query
+                connection.query(sqlQuery, function (err, rows) {
+                        console.log('State 2 - inside query ' + connection.state);
+                        // resolve or reject promise asynchronously
+                        if (err) reject(err);
+                        else resolve(rows);
+                });
+                console.log('State 3 - after query ' + connection.state);
+                connection.end();
+        });
+        // return synchronous promise
+        return promise;
 }
 
-exports.getSingleRecord = function(sqlQuery, callback)
-{
-        var connection = connectionStart();
-        //console.log(callback);
+exports.getSingleRecord = function (sqlQuery, callback) {
+        var promise = new Promise(function (resolve, reject) {
 
-        connection.connect();
-        connection.query(sqlQuery, function(err, rows, fields){
-                console.log(sqlQuery);
-                if (err)
-                {
-                  callback(err,null);
-                }
-                else
-                {
-                 //console.log('ROWS ' + rows);
-                 //console.log('FIELDS ' + fields[0]);
-                 callback(null, rows[0]);
-                }
+                var connection = connectionStart();
+                //console.log(callback);
+
+                connection.connect();
+
+                // create asynchronous query
+                connection.query(sqlQuery, function (err, rows, fields) {
+                        console.log(sqlQuery);
+                        // resolve or reject promise asynchronously
+                        if (err) reject(err);
+                        else resolve(rows[0]);
+                });
+                connection.end();
         });
-        connection.end();
+        // return synchronous promise
+        return promise;
 }
